760e13b62586991d852a10f07ceaa8c0f2e16674
asingh34 <33998240+asingh34@users.noreply.github.com>
2019-03-01 21:52:00 -0500

    Add files via upload

---
+++ anant.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Anant Singh | Assignment 5</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body class = container>
+   <header>
+         <img src ="images/education.jpg" alt="ant">
+         <h1 class = name> Education</h1>
+
+   </header>
+    <?php include "inc/nav.inc";?>
+
+    <aside>
+      <ul>
+         <li>Anant Singh</li>
+         <hr>
+         <li>Akshay Desai</li>
+         <hr>
+         <li>Elon Musk</li>
+         <hr>
+      </ul>
+   </aside>
+
+
+
+   <main>
+
+   <div class = block>
+      <h2>Anant Singh</h2>
+      <hr>
+         <div class = early>
+      <p>I graduated from U-Prep, Seattle WA in 2016 and am currently enrolled at the <em>University of Rochester</em>. I am currently a Junior and have switched my major 4 times while here. I was admitted as Mechanical Engineer, switched to optics, switched to economics, and finally settled with computer science. I played Rugby at the UofR my freshman year, and now play Lacrosse. I am also a brother of the Sigma Chi Fraternity and Design Chairman. </p>
+
+      <p> I came to appreciate computer science as it was the first time in a major I found I was able to bring in other passions like art, building, design so early in a major, and was really fascinated by my discrete math course. I was then enrolled into more code heavy courses like Introduction to Computer Science (Java), Web Design and Development, and Data Structures and Algorithms. These classes were where I really started to feel like I could bring in my passion for imagining, designing, and building things. Simultaneously I was taking studio arts classes. Although the final products were in an abstract form in terms of programs, there was a certain satisfaction with completing a project end to end and having everything work which made it feel less abstract and more real than ever. I was creating responsive programs and was able to stylize and design how it all looked end to end. My Current computer science courses are Advanced Front-end Web Design, Formal Systems and Computations, and Writing for Computer Science.</p>
+         </div>
+   </div>
+
+   <div class = block>
+      <h2>Akshay Desai</h2>
+      <hr>
+      <div class = ed>
+      <p> I took many interesting courses in computer science. I started from the beginning, taking “The Science of Programming”, The <em>University of Rochester’s</em> introduction class to computer science. Since then, I’ve taken many classes and even took more classes than were required for the major. A few notable ones are “Operating Systems”, Advanced Algorithms”, “Web Development”, and “Android Development”; in these classes, I’ve learned many concepts and techniques important to being a software engineer. I also have been a Workshop Leader/ Teaching Assistant beginning my second semester of college. I led workshops for “The Science of Programming” and then “Data Structures and Algorithms”. Later in my academic career, I became a Teaching Assistant for “Computer Organization”. Along with those leader experiences, I also am a Resident Advisor (RA), where I live with my residents and ensure a happy living environment which is both safe and enjoyable. In my spare time, other than coding, I dance on a competitive dance team called Rochester Bhangra. It is an Indian dance from the state of Punjab and we travel to competitions every year; Toronto, Chicago, DC, and NYC to name a few.</p>
+   </div>
+
+   <div class = block>
+      <h2>Elon Musk</h2>
+      <hr>
+      <div class = job>
+        <p>Born and raised in Pretoria, South Africa, Musk moved to Canada when he was 17 to attend Queen's University. He transferred to the University of Pennsylvania two years later, where he received an economics degree from the Wharton School and a degree in physics from the College of Arts and Sciences. He began a Ph.D. in applied physics and material sciences at Stanford University in 1995 but dropped out after two days to pursue an entrepreneurial career. He subsequently co-founded Zip2, a web software company, which was acquired by Compaq for $340 million in 1999. Musk then founded X.com, an online bank. It merged with Confinity in 2000 and later that year became PayPal, which was bought by eBay for $1.5 billion in October 2002</p>
+      </div>
+   </div>
+
+   </main>
+
+
+
+   <?php include "inc/footer.inc";?>
+
+   <nav class = next>
+      <ul>
+         <li> <a href = "index.php">Back</a></li>
+      </ul>
+   </nav>
+
+
+    <script src="http://code.jquery.com/jquery.js"></script>
+   <script src="js/menu-highlighter.js"></script>
+</body>
+</html>
